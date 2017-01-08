8a9330b8902961cf039dd855af3f6d9f4399c6da
Ricky Grassmuck <rigrassm@gmail.com>
2017-01-08 02:04:21 -0600

    Cleaned up indention and converted to new print function

--- socket_server.py
+++ socket_server.py
@@ -1,75 +1,71 @@
 #!/usr/bin/python
-
+from __future__ import print_function
 import socket
-import sys, getopt
+import sys
+import getopt
 from thread import *
 
-#Function for handling connections. This will be used to create threads
+
 def clientthread(conn, port):
-    HOSTNAME = socket.gethostname()
+    hostname = socket.gethostname()
 
-    #Sending message to connected client
-    conn.send('Connected to ' + HOSTNAME + ' on port ' + port + '. Type something and hit enter\n') #send only takes string
+    conn.send('Connected to {host} on port {port} Type something and hit enter\n'.format(host=hostname, port=port))
 
-    #infinite loop so that function do not terminate and thread do not end.
     while True:
-
-        #Receiving from client
         data = conn.recv(1024)
-        reply = 'OK...' + data
+        reply = 'OK... {}'.format(data)
         if not data:
             break
 
         conn.sendall(reply)
 
-    #came out of loop
     conn.close()
 
+
 def main(argv):
-	HOST = ''   # Symbolic name meaning all available interfaces
-	PORT = '' # Arbitrary non-privileged port
+    host = ''
+    port = ''
 
-	try:
-	  opts, args = getopt.getopt(argv,"hp:",["port="])
-	except getopt.GetoptError:
-	  print 'test.py -p <PORT>'
-	  sys.exit(2)
-	for opt, arg in opts:
-	  if opt == '-h':
-		 print 'test.py -p <PORT>'
-		 sys.exit()
-	  elif opt in ("-p", "--port"):
-		 if isinstance(int(arg), (int, long)):
-			PORT = int(arg)
-		 else:
-			print "Port argument must be an integer"
+    try:
+        opts, args = getopt.getopt(argv, "hp:", ["port="])
+    except getopt.GetoptError:
+        print('test.py -p <PORT>')
+        sys.exit(2)
 
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	print 'Socket created'
+    for opt, arg in opts:
+        if opt == '-h':
+            print('test.py -p <PORT>')
+            sys.exit()
 
-	#Bind socket to local host and port
-	try:
-		s.bind((HOST, PORT))
-	except socket.error as msg:
-		print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-		sys.exit()
+        elif opt in ("-p", "--port"):
 
-	print 'Socket bind complete'
+            if isinstance(int(arg), (int, long)):
+                port = int(arg)
 
-	#Start listening on socket
-	s.listen(10)
-	print 'Socket now listening on port ' + str(PORT)
-	
-	#now keep talking with the client
-	while 1:
-		#wait to accept a connection - blocking call
-		conn, addr = s.accept()
-		print 'Connected with ' + addr[0] + ':' + str(addr[1])
+        else:
+            print("Port argument must be an integer")
 
-		#start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
-		start_new_thread(clientthread ,(conn, str(PORT),))
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print('Socket created')
+
+    try:
+        s.bind((host, port))
+    except socket.error as msg:
+        print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+        sys.exit()
+
+    print('Socket bind complete')
+
+    s.listen(10)
+    print('Socket now listening on port ' + str(port))
+
+    while 1:
+        conn, addr = s.accept()
+        print('Connected with ' + addr[0] + ':' + str(addr[1]))
+
+        start_new_thread(clientthread, (conn, str(port),))
         s.close()
-		
+
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
